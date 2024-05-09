89e93523150861aab1190f9ca28d194918d5c7dc
nam-yeun-hwa <nyh2ya@gmail.com>
2024-05-09 11:48:54 +0900

    Feat(Select):Focus 이벤트 적용에 따른 css border 변경

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { selectOptionType } from "type/data";
 import "./select.css";
 
@@ -10,17 +10,66 @@ type SelectProps = {
 
 export default function Select(props: SelectProps) {
   const { value, options, onChange } = props;
+  const [isFocused, setIsFocused] = useState(false);
 
+  const [inputValue, setInputValue] = useState("");
+  const [optionSearchList, setOptionSearchList] = useState<
+    selectOptionType | undefined
+  >();
+
+  /**
+   * @description input검색에 따른 option filter
+   */
   useEffect(() => {
-    console.log("components options", options);
-  }, [options]);
+    if (inputValue.length > 0) {
+      const filteredItems = options?.filter((optionItem) => {
+        return optionItem.label.includes(inputValue);
+      });
+      if (filteredItems && filteredItems?.length > 0) {
+        setOptionSearchList(filteredItems);
+      }
+    }
+  }, [inputValue, setOptionSearchList]);
+
+  /**
+   * @function handleInputChange
+   * @description select option 검색
+   */
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  /**
+   * @function handleFocus
+   * @description 포커스 이벤트 핸들러
+   */
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  /**
+   * @function handleBlur
+   * @deprecated 포커스 아웃 이벤트 핸들러
+   */
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+
   return (
     <>
       <div className="MuiAutocomplete-root">
         <div className="MuiFormControl-root">
           {/* <label className="MuiFormLabel-root">Movie</label> */}
-          <div className="MuiInputBase-root">
-            <input className="MuiInputBase-input" placeholder={"Movie"} />
+          <div className={`MuiInputBase-root ${isFocused && "focused"}`}>
+            <input
+              className="MuiInputBase-input"
+              type="text"
+              value={inputValue}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              onChange={handleInputChange}
+              placeholder={"Movie"}
+            />
             <div className="MuiAutocomplete-endAdornment">
               <button className="MuiButtonBase-root">
                 <svg>
@@ -32,9 +81,23 @@ export default function Select(props: SelectProps) {
         </div>
       </div>
       <div className="base-popper-root">
-        {options?.map((item) => {
-          return <div className="option-item">{item.label}</div>;
-        })}
+        {inputValue && inputValue?.length > 0 ? (
+          <>
+            {optionSearchList?.map((item) => {
+              return <div className="option-item">{item.label}</div>;
+            })}
+          </>
+        ) : (
+          <>
+            {options?.map((item) => {
+              return (
+                <div className="option-item">
+                  [{item.value}]-{item.label}
+                </div>
+              );
+            })}
+          </>
+        )}
       </div>
     </>
   );
