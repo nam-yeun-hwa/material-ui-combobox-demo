5dbaf445633bf39bfb6b637a0d363681aa0c6e54
nam-yeun-hwa <nyh2ya@gmail.com>
2024-05-22 20:08:38 +0900

    Remove(폴더삭제): asset 폴더 삭제

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,29 +1,13 @@
-import {
-  ChangeEvent,
-  Children,
-  KeyboardEvent,
-  ReactNode,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
-import { optionItem, selectOptionType } from "type/data";
+import { ReactNode, useRef, useState } from "react";
 import "./select.css";
-
-import useWindowSize from "../hook/useWindowSize.ts";
-import { SELECT_OPTION } from "../constant/constant";
 import Button from "./Button.tsx";
-import OptionList from "./OptionList.tsx";
 
 type SelectProps = {
   children: ReactNode;
   isFocused: Boolean;
   isToggle: Boolean;
-  // onSelect: (select: optionItem) => void;
   inputValue: string;
-  // options: selectOptionType;
   onChange: (value: string) => void;
-  // onHover: (select: optionItem) => void;
   onFocus: () => void;
   onBlur: () => void;
   onClick: () => void;
@@ -36,13 +20,10 @@ export default function Select({
   children,
   isFocused,
   isToggle,
-  // onSelect,
   inputValue,
-  // options,
   onChange,
   onFocus,
   onBlur,
-  // onHover,
   onToggle,
   onClear,
   onClick,
@@ -77,166 +58,6 @@ export default function Select({
   //   }
   // }, [selectRef, windowSize]);
 
-  /**
-   * @description input검색에 따른 option filter
-   */
-  // useEffect(() => {
-  //   if (inputValue.length > 0) {
-  //     const filteredItems = options?.filter((optionItem) => {
-  //       return optionItem.label.includes(inputValue);
-  //     });
-
-  //     if (filteredItems && filteredItems?.length > 0) {
-  //       setOptionSearchList(filteredItems);
-  //     } else {
-  //       setOptionSearchList([{ label: "No options", value: "0" }]);
-  //     }
-  //   }
-  // }, [inputValue, setOptionSearchList]);
-
-  /**
-   * @description selectOptionActive 초기화 시켜주기
-   * option 클릭 후 input 해당 옵션값의 텍스트가 들어가 있을때 옵션 값을 다 지워줄 경우에는
-   * selectOptionActive를 false로 초기화 해준다.
-   */
-  // useEffect(() => {
-  //   if (inputValue.length === 0) {
-  //     setSelectOptionActive(undefined);
-  //     setIsOptionToggle(true);
-  //   }
-  // }, [inputValue]);
-
-  /**
-   * @function handleInputChange
-   * @description select option 검색
-   */
-  // const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-  //   setInputValue(event.target.value);
-  // };
-
-  /**
-   * @function onFocusHandler
-   * @description 포커스 이벤트 핸들러
-   */
-  // const onFocusHandler = () => {
-  //   setIsFocused(true);
-  //   setIsOptionToggle(true);
-  // };
-
-  /**
-   * @function onBlurHandler
-   * @deprecated 포커스 아웃 이벤트 핸들러
-   */
-  // const onBlurHandler = () => {
-  //   setIsFocused(false);
-  //   setInputValue("");
-  // };
-
-  /**
-   * @function onClickOptionValue
-   * @param optionValue 클릭한 option item
-   * @description 클릭한 옵션
-   */
-  // const onClickOptionItem = (optionItem: optionItem) => {
-  //   setIsOptionToggle(false);
-  //   if (optionItem) {
-  //     setInputValue(optionItem.label);
-  //     setSelectOptionActive(optionItem);
-  //   }
-  // };
-
-  /**
-   * @function onClickClearInputSearch
-   * @description input search clear
-   */
-  // const onClickClearInputSearch = () => {
-  //   setInputValue("");
-
-  //   setIsFocused(true);
-  //   setOnEnterItem(undefined);
-  //   setSelectOptionActive(undefined);
-  //   if (inputRef.current) {
-  //     inputRef.current.focus();
-  //   }
-  // };
-
-  /**
-   * @function onClickFocusOnHandler
-   * @description input 클릭시 Focus On
-   */
-  // const onClickFocusOnHandler = () => {
-  //   setIsFocused(true);
-  //   setIsOptionToggle(true);
-  // };
-
-  /**
-   * @function onToggleOptionList
-   * @description option toggle
-   */
-  // const onToggleOptionList = () => {
-  //   // setIsFocused(!isFocused);
-  //   setIsOptionToggle(!isOptionToggle);
-  // };
-
-  /**
-   * @function onMouseOverHandler
-   * @param optionItem
-   */
-  // const onMouseOverHandler = (optionItem: optionItem) => {
-  //   setOnEnterItem(optionItem);
-  // };
-  /**
-   * @function onKeyboardHandler
-   * @description 키보드 이벤트
-   */
-  // const onKeyboardHandler = (e: KeyboardEvent<HTMLDivElement>) => {
-  //   switch (e.key) {
-  //     case "Enter":
-  //       console.log("Enter 키가 눌렸습니다.");
-  //       if (enterItem) {
-  //         onClickOptionItem(enterItem);
-  //       }
-  //       break;
-  //     case "ArrowUp":
-  //       console.log("위쪽 화살표 키가 눌렸습니다.");
-  //       if (isOptionToggle) {
-  //         setOnEnterItem((prevState) => {
-  //           if (prevState) {
-  //             const currentValue = Number(prevState?.value);
-  //             return options?.find(
-  //               (item) => item.value === String(currentValue - 1)
-  //             );
-  //           } else {
-  //             return options && options[99];
-  //           }
-  //         });
-  //       } else {
-  //         setIsOptionToggle(true);
-  //       }
-
-  //       break;
-  //     case "ArrowDown":
-  //       if (isOptionToggle) {
-  //         setOnEnterItem((prevState) => {
-  //           if (prevState) {
-  //             const currentValue = Number(prevState?.value);
-  //             return options?.find(
-  //               (item) => item.value === String(currentValue + 1)
-  //             );
-  //           } else {
-  //             return options && options[0];
-  //           }
-  //         });
-  //       } else {
-  //         setIsOptionToggle(true);
-  //       }
-  //       console.log("아래쪽 화살표 키가 눌렸습니다.");
-
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // };
   return (
     <>
       <div
@@ -311,16 +132,7 @@ export default function Select({
           }`}
         >
           <div className="base-popper-content" ref={optionRef}>
-            {
-              isToggle && children
-              // <OptionList
-              //   onkeyboard={onkeyboard}
-              //   options={options}
-              //   onSelect={onSelect}
-              //   onHover={onHover}
-              // />
-            }
-            {/* {<div>test</div>} */}
+            {isToggle && children}
           </div>
         </div>
       </div>
